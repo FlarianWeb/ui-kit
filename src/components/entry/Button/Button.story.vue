2621955cbd1d133c7960d36d93f714bd11ec0407
Ramil Musin <musin@flarian.ru>
2025-08-28 21:03:04 +0300

    feat: :rocket: Init project
    Add starter build

---
+++ src/components/entry/Button/Button.story.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Button from "./Button.vue";
+</script>
+
+<template>
+  <Story title="Button" :layout="{ type: 'grid', width: 300 }">
+    <Variant title="Primary">
+      <Button label="Primary Button" />
+    </Variant>
+    <Variant title="Secondary">
+      <Button label="Secondary Button" variant="secondary" />
+    </Variant>
+    <Variant title="Danger">
+      <Button label="Danger Button" variant="danger" />
+    </Variant>
+    <Variant title="Disabled">
+      <Button label="Disabled Button" :disabled="true" />
+    </Variant>
+  </Story>
+</template>
